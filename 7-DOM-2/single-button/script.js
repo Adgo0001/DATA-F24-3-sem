a0d47ebfd78f21d43e65d0efc65a4eb95fd6629d
Adrian Goldbech <adgo0001@stud.kea.dk>
2025-02-17 16:21:52 +0100

    hs

--- 7-DOM-2/single-button/script.js
+++ 7-DOM-2/single-button/script.js
@@ -1,8 +1,12 @@
 'use strict'
 
-const button = document.querySelector("#spinButton");
+const dontClickMeButton = document.querySelector("#spinButton");
 
 const testButton = document.querySelector("#test");
+
+const clickMeButton = document.querySelector("#click-me")
+
+const dontClickMeButton = document.querySelector("#message").textContent = "Message";
 
 testButton.addEventListener("click", () =>{
     console.log("Hey du klikkede knappen!!!!");
@@ -12,7 +16,26 @@ let timesPressed = 0;
 
 //Add event listener to the test button 
 
-button.addEventListener("click", () => {
+clickMeButton.addEventListener("click", () => {
+    console.log("Ah thank you");
+    timesPressed += 1;
+    console.log(timesPressed);
+})
+
+//If the button was pressed 1 time: Print to the console: "Please do not do that again"
+//If the button was pressed 2 times: print to the console: I told you not to do that
+//If the button was pressed 3 times print to the console STOP!!!
+dontClickMeButton.addEventListener("click", () => {
     //This spins around the button
     document.getElementById("spinButton").style.transform += "skew(-50deg, 25deg) rotate(270deg)";
+
+    timesPressed += 1;
+
+    if (timesPressed == 1) {
+        console.log("Please do not do that again...")
+    } else if (timesPressed == 2) {
+        console.log("I told you not to do that!")
+    } else if (timesPressed == 3) {
+        console.log("STOP!!")
+    }
 });
